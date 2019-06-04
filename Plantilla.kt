76d20448133ecfa32d270b22add4628b163fc177
finfanterodal <finfanterodal@danielcastelao.org>
2019-06-04 09:52:48 +0200

    Cambio del constructor de la clase Plantilla, de manera que se simplifica.

--- Plantilla.kt
+++ Plantilla.kt
@@ -2,23 +2,8 @@
 1. Los Getter y Setter no son necesarios en kotlin y el acceso a los atributos se hace como obx.nombre="Pepe"
 2. Hay dos tipos de constructores, uno donde le definimos los valores
  */
-open class Plantilla {
+open class Plantilla (var id: Int,var edad: Int,var nombre: String,var apellido: String)  {
 
-
-    var id: Int = 0
-    var edad: Int = 0
-    var nombre: String? = null
-    var apellido: String? = null
-    //Constructores
-
-    constructor() {}
-    //Constructor secundario
-    constructor(id: Int, edad: Int, nombre: String, apellido: String) {
-        this.id = id
-        this.edad = edad
-        this.nombre = nombre
-        this.apellido = apellido
-    }
 
     //Métodos
     fun concentrarse() {
